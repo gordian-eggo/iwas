64e367a31b2ab977d7333b89c09dfc8480f47f17
seraph <columbus.knot@gmail.com>
2018-06-02 21:31:04 +0800

    Added Board to Main class

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ import java.util.*;
 public class Main {
 	public static void main(String []args) {
 		System.out.println("Program initialization successful.");
+		Game dodger = new Game();
+		System.out.println("Operational.");
 	}
 }
