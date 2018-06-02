6e96c03d076fefe487777400a6eccee499a836b3
seraph <columbus.knot@gmail.com>
2018-06-03 02:27:42 +0800

    Latest GUI changes

--- GUI.java
+++ GUI.java
@@ -1,17 +1,38 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.Dimension;
-import java.awt.Container;
-import java.awt.Color;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.lang.Thread;
 
 public class GUI {
-	
+
+	public GUI() {
+		Frame frame = new Frame("Iwas");
+
+		Container contain = frame.getContentPane();
+		contain.setLayout(new BorderLayout());
+		
+		Container container = frame.getContentPane();
+		container.setLayout(new BorderLayout());
+
+		GamePanel center = new GamePanel();
+		center.setLayout(new BorderLayout());
+		center.setBackground(Color.BLACK);
+
+		JButton start = new JButton("Start Game");
+
+		start.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Game start");
+			}
+		});
+
+		center.add(start, BorderLayout.CENTER);
+
+		container.add(center, BorderLayout.CENTER);
+
+		frame.pack();
+		frame.setVisible(true);
+
+	}
+
 }
---
+++ GamePanel.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.util.*;
+
+public class GamePanel extends JPanel {
+
+	public GamePanel() {
+		
+	}
+}
